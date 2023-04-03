cad9fe7426c9cf008b353dba3df2dfdf832cd012
Jade Stewart <jadekstewart3@gmail.com>
2023-04-03 14:56:44 -0600

    Add routes to allow for login

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "landing#index"
 
-  resources :users, only: %i[show create] do
+  resources :users, only: [:show, :new, :create] do
     resources :discover, only: [:index]
     resources :movies, only: %i[index show] do
       resources :viewing_party, only: %i[new create]
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   end
 
   get "/register", to: "users#new"
+  get "/login", to: "users#login_form"
+  post "/login", to: "users#login_user"
 end
