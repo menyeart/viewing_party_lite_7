0efdba32299d57b75ec5c94664a98ec08038d66f
Jade Stewart <jadekstewart3@gmail.com>
2023-04-04 18:40:56 -0600

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   get "/login", to: "users#login_form"
   post "/login", to: "users#login_user"
+  get "/logout", to: "users#logout"
 end
